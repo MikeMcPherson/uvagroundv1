43b3e7718fc74c879b5d01bf5c181587711778b9
MikeMcPherson <mcpherson@acm.org>
2019-06-14 18:49:24 -0400

    Update database format

--- database/LibertasReadonly.sql
+++ database/LibertasReadonly.sql
@@ -1,4 +1,4 @@
-DROP USER LibertasReadonly;
-CREATE USER 'LibertasReadonly' IDENTIFIED BY 'G0ing2sPace!' REQUIRE SSL;
-GRANT USAGE ON *.* TO LibertasReadonly;
+DROP USER IF EXISTS LibertasReadonly;
+CREATE USER 'LibertasReadonly' IDENTIFIED BY 'G0ing2sPace!';
+GRANT USAGE ON *.* TO LibertasReadonly REQUIRE SSL;
 GRANT SELECT ON TABLE `LibertasTest`.* TO 'LibertasReadonly';
