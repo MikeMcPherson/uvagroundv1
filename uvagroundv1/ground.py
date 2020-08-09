0dcd54a519bc4c3b1b63bea6ae8084e4a8e41fbc
Mike McPherson <mcpherson@acm.org>
2020-08-09 09:57:17 -0400

    Fix error in copyright statement

--- uvagroundv1/ground.py
+++ uvagroundv1/ground.py
@@ -9,18 +9,18 @@ GNU Radio flowgraph that controls the radio, communicating with GNU Radio
 via a TCP socket.  It also communicates with the sequencer over the local
 network via a REST interface.
 
-Copyright 2018, 2019 by Michael R. McPherson, Charlottesville, VA
+Copyright 2018, 2019, 2020 by Michael R. McPherson, Charlottesville, VA
 mailto:mcpherson@acm.org
 http://www.kq9p.us
 
 This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License as published by the Free
-Software Foundation; either version 2 of the License, or (at your option) any
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3 of the License, or (at your option) any
 later version.
 
 This program is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+PARTICULAR PURPOSE.  See the GNU General Public License for more
 details.
 
 You should have received a copy of the GNU General Public License along with
--- uvagroundv1/utilities/calibrate_tx.py
+++ uvagroundv1/utilities/calibrate_tx.py
@@ -3,18 +3,18 @@
 """
 Simple ground station for the UVa Libertas spacecraft.
 
-Copyright 2018 by Michael R. McPherson, Charlottesville, VA
+Copyright 2018, 2020 by Michael R. McPherson, Charlottesville, VA
 mailto:mcpherson@acm.org
 http://www.kq9p.us
 
 This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License as published by the Free
+the terms of the GNU General Public License as published by the Free
 Software Foundation; either version 2 of the License, or (at your option) any
 later version.
 
 This program is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+PARTICULAR PURPOSE.  See the GNU General Public License for more
 details.
 
 You should have received a copy of the GNU General Public License along with
