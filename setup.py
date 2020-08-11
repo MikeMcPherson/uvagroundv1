bf2e81b40dca8f21b4a5bf3ad83e58987c6b795e
Mike McPherson <mcpherson@acm.org>
2020-08-10 20:21:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
-from setuptools import setup
+import setuptools
 
-setup(name='ground',
-      version='1.0',
-      description='UVa Libertas Ground Station',
-      url='https://github.com/MikeMcPherson/UVaGroundStation',
-      author='Michael R. McPherson',
-      author_email='mcpherson@acm.org',
-      license='GNU General Public License v3 (GPLv3)',
-      packages=['ground'],
-      zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name="uvagroundv1",
+      version="1.6",
+      author="Michael R. McPherson",
+      author_email="mcpherson@acm.org",
+      description="UVa Libertas Ground Station",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/MikeMcPherson/uvagroundv1",
+      packages=setuptools.find_packages(),
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6',
+)
